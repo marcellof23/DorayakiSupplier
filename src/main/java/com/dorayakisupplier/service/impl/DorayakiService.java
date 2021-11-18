af8be183cc68fc5f2ae3e185f8b74dfb1bef5257
Marcello Faria <01faria.marcello@gmail.com>
2021-11-18 15:51:43 +0700

    push wsdl

---
+++ src/main/java/com/dorayakisupplier/service/impl/DorayakiService.java
@@ -0,0 +1,28 @@
+package com.dorayakisupplier.service.impl;
+
+import com.dorayakisupplier.model.DorayakiVariant;
+import com.dorayakisupplier.repo.DorayakiVariantRepo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
+public class DorayakiService {
+
+    private static final DorayakiVariantRepo dorayakiRepo = new DorayakiVariantRepo();
+
+    @WebMethod
+    public List<DorayakiVariant> getAllDorayaki(){
+        try {
+            return dorayakiRepo.getDorayakiVariants();
+        } catch (Exception e){
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+}
