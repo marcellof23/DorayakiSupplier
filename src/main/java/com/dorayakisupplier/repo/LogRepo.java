0c37d5fff4c4c1329a757d165b279c0fe2e88e02
Frederic Ronaldi <frederic.ronaldi@gmail.com>
2021-11-24 19:12:21 +0700

    implement rate limiting

--- src/main/java/com/dorayakisupplier/repo/LogRepo.java
+++ src/main/java/com/dorayakisupplier/repo/LogRepo.java
@@ -20,7 +20,8 @@ public class LogRepo {
     public Boolean addLog(LogType logType) throws SQLException {
         try {
             Statement smt = this.conn.createStatement();
-            String sql = "INSERT INTO logrequest values(NULL, " + logType.getIp() + ", " + logType.getEndpoint() + ", " + logType.getTimestamp() + ", " + logType.getTimestamp() + ");";
+            String sql = "INSERT INTO logrequest values(NULL, '" + logType.getIp() + "', '" + logType.getEndpoint() + "', '" + logType.getTimestamp() + "', '" + logType.getTimestamp() + "');";
+            sql = sql.replace("\"", "");
             smt.executeUpdate(sql);
             return true;
         } catch (Exception e){
@@ -43,12 +44,13 @@ public class LogRepo {
     }
 
     public int countLog(String ip, Date start, Date end) throws SQLException {
-        SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+        SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         String sql = "SELECT COUNT(*) as log_count FROM logrequest " +
                 "WHERE ip = '" + ip + "' AND created_at between '" +
-                f.format(start) +"' and '" + f.format(end) + "'";
+                f.format(start) + "' and '" + f.format(end) + "'";
         ResultSet rs = this.conn.createStatement().executeQuery(sql);
-        return rs.getInt("log_count");
+        if (rs.next()) return rs.getInt("log_count");
+        return 0;
     }
 }
 
